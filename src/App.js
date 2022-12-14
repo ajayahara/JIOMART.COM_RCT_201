535a8ff7f56f2a9bdf5855f2d130dc02e0abdaa6
Rashmiranjan117 <107473816+Rashmiranjan117@users.noreply.github.com>
2022-12-14 22:12:53 +0530

    added indivisualpage with login in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import IndivisualPage from './Pages/IndivisualPage';
 import Login from './Pages/login';
 
 function App() {
   return (
     <div className="App">
       <Login />
+      <IndivisualPage />
     </div>
   );
 }
