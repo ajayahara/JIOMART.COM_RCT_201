aec3b017ac27e68ecb94c3028e89129b247d23bc
ajayahara <abak00350@gmail.com>
2022-12-15 18:29:38 +0530

    fw19_0841

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@
 
 import './App.css';
 import Login from './Pages/login';
-import { CartLoginItems } from './Pages/Cart/CartLoginItems';
 function App() {
   return (
     <div className="App">
-      {/* <Login /> */}
-      <CartLoginItems/>
+      <Login />
     </div>
   );
 }
