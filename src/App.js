612b940b29d4a79e03fab3028773865a32405be8
Rashmiranjan117 <107473816+Rashmiranjan117@users.noreply.github.com>
2022-12-15 20:33:38 +0530

    added product page with sorting functionality and add to cart functionality and loading status

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 
 import './App.css';
 import Login from './Pages/login';
+import Products from './Pages/Products';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      {/* <Login /> */}
+      <Products />
     </div>
   );
 }
