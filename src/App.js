bce10846a16230bab6ef239e9a80bab41f1a147e
ajayahara <abak00350@gmail.com>
2022-12-18 02:15:24 +0530

    ajaya

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@
 
 import './App.css';
 import Login from './Pages/login';
+
+import {Address} from "./Pages/Cart/Address"
 function App() {
   return (
     <div className="App">
-      <Login />
+      {/* <Login /> */}
+    <Address/>
     </div>
   );
 }
