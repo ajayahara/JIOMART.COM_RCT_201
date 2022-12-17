9344806724e7c41cbc4ee33f06d3f3edf69285bd
ajayahara <abak00350@gmail.com>
2022-12-17 19:45:03 +0530

    17.12

---
+++ src/Pages/Cart/Address.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { AddressItem } from '../../Component/Cart/AddressItem'
+import { CartNav } from '../../Component/Cart/CartNav'
+import { DeliveryAddress } from '../../Component/Cart/DeliveryAddress'
+import { NoAddress } from '../../Component/Cart/NoAddress'
+import { PaymentDetils } from '../../Component/Cart/PaymentDetils'
+import "../Cart/Address.css"
+export const Address = () => {
+  return (
+    <div className='Address'>
+      <CartNav />
+      <div>
+        <div>Order Summary</div>
+        <div></div>
+      </div>
+      <div>
+        <div>
+          <NoAddress/>
+          <div className='basket'>
+            <div>
+              <div>Groceries Basket <span>(1 items)</span></div>
+              <div>₹129.00</div>
+            </div>
+            <div>
+              <AddressItem />
+            </div>
+          </div>
+        </div>
+        <div>
+          <PaymentDetils/>
+          <div className='PaymentButton'><button>Make Payment</button></div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
