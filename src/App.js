263968c3c049328757ae916028bbb61760d07060
Rashmiranjan117 <107473816+Rashmiranjan117@users.noreply.github.com>
2022-12-18 21:36:47 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import './App.css';
+import "./App.css";
 
-import Home from './Component/Home';
+import Home from "./Component/Home";
 
-
-
-import Nav from './Pages/Navbar';
-import Navbar from './Pages/IndivisualPage'
-import AllRoutes from './Routes/AllRoutes';
-import Footer from './Pages/footer';
-import Login from './Pages/login';
+import Nav from "./Pages/Navbar";
+import Navbar from "./Pages/IndivisualPage";
+import AllRoutes from "./Routes/AllRoutes";
+import Footer from "./Pages/footer";
+import Login from "./Pages/login";
 function App() {
   return (
     <div className="App">
-      {/* <Nav />
+      <Nav />
       <AllRoutes />
-      <Footer /> */}
-
+      <Footer />
     </div>
   );
 }
 
 export default App;
-
