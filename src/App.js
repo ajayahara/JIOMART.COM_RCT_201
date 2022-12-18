2c1b70cd6600eee9118d891dd3a076bf464814bb
Rashmiranjan117 <107473816+Rashmiranjan117@users.noreply.github.com>
2022-12-18 17:31:18 +0530

    conflicts resolved

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 import './App.css';
-<<<<<<< HEAD
+
 import Home from './Component/Home';
-import IndivisualPage from './Pages/IndivisualPage';
+
 
 
 import Nav from './Pages/Navbar';
 import Navbar from './Pages/IndivisualPage'
 import AllRoutes from './Routes/AllRoutes';
 import Footer from './Pages/footer';
-localStorage.getItem("otp",JSON.stringify())
-function App() {
-  return (
-    <div className="App">
-      <Nav />
-      {/* <Home/> */}
-      <AllRoutes />
-      {/* <Footer/> */}
-      {/* <IndivisualPage /> */}
-      {/* <Navbar /> */}
-=======
 import Login from './Pages/login';
 function App() {
   return (
     <div className="App">
       <Login />
->>>>>>> ce307e228988c4c58ff3826e6fc1196355796119
+
     </div>
   );
 }
