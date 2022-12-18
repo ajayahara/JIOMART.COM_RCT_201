6f1f37f9a02f97cd1fe573f8a6ec9eb2c3d7c28b
ajayahara <abak00350@gmail.com>
2022-12-19 01:36:21 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -9,12 +9,16 @@ import Navbar from './Pages/IndivisualPage'
 import AllRoutes from './Routes/AllRoutes';
 import Footer from './Pages/footer';
 import Login from './Pages/login';
+import { Address } from './Pages/Cart/Address';
+import {CartLoginItems} from "./Pages/Cart/CartLoginItems"
 function App() {
   return (
     <div className="App">
       {/* <Nav />
       <AllRoutes />
       <Footer /> */}
+      {/* <Address/> */}
+      <CartLoginItems/>
     </div>
   );
 }
