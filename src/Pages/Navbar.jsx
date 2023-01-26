bb7fe1bf9629d0ffaeb8703b7f11fc67e7a6ea39
Puneet Srivastava <srivastavap714@gmail.com>
2023-01-27 03:44:47 +0530

    Added Routes, Css and Anmin Page Functionality

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import * as FaIcons from "react-icons/fa";
-// import logo from "../Resources/smstr.png";
+import { RiAdminLine } from "react-icons/ri";
 import logo from "../Resources/bcg-noBackground.png";
-// import logo from "../Resources/sslogo.png";
+import { BiLogOut } from "react-icons/bi";
+import { FiShoppingCart } from "react-icons/fi";
 import "./Navbar.css";
 import { IconContext } from "react-icons";
 import {
@@ -30,6 +31,7 @@ import { useNavigate } from "react-router";
 import { useSelector } from "react-redux";
 import axios from "axios";
 import { AddIcon, EditIcon, ExternalLinkIcon, RepeatIcon } from "@chakra-ui/icons";
+import { AiOutlineDown } from "react-icons/ai";
 function Navbar() {
   const [sidebar, setSidebar] = useState(false);
   const navigate = useNavigate();
@@ -50,6 +52,9 @@ function Navbar() {
       navigate("/login")
     }
   }
+  const handleLogout = () => {
+    localStorage.removeItem("Jio Mart User");
+  };
   return (
     <Box>
       <Box style={{ display: "flex", justifyContent: "space-evenly" }}>
@@ -90,49 +95,17 @@ function Navbar() {
               />
             </InputGroup>
           </div>
-          <Flex color={"white"} ml={"30px"} gap={2}>
-            {/* <Person2RoundedIcon  /> */}
-            <Center gap={2}>          
-            {val!=null?
-              <Menu>
-  <MenuButton
-    as={IconButton}
-    aria-label='Options'
-    icon={ <Person2RoundedIcon  />}
-    variant='outline'
-  />
-  <MenuList boxShadow="rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px">
-    <MenuItem icon={<AddIcon />} >
-      My Order
-    </MenuItem>
-    <MenuItem icon={<ExternalLinkIcon />} >
-      My Cart
-    </MenuItem>
-    <MenuItem icon={<RepeatIcon />} >
-      Admin Panel
-    </MenuItem>
-    <MenuItem icon={<EditIcon />} >
-      LogOut
-    </MenuItem>
-  </MenuList>
-</Menu>:""}
-<p className="R-Text" display={{base:"none", md:"block"}} style={{ cursor: "pointer" }} onClick={handleLogin}>
-              {val || "Sign in/ Sign Up"}
-             
-            </p>
-</Center>
- 
-            <Flex
+          <Flex
               gap={{ base: 1, md: 2, lg: 4 }}
               color="white"
               style={{ cursor: "pointer" }}
                 className='R-icons-top'
             >
-              <ShoppingCartRoundedIcon onClick={() => navigate("/cart")} />
+              <ShoppingCartRoundedIcon  onClick={() => navigate("/cart")} />
               <Box
                 style={{
                   position: "absolute",
-                  right: "7%",
+                  right: "20.6%",
                   backgroundColor: "red",
                   color: "white",
                   borderRadius: "40px",
@@ -141,10 +114,11 @@ function Navbar() {
                   fontSize: "13px",
                   lineHeight: "13px",
                   textAlign: "center",
-                  top: "25px",
+                  top: "24px",
                 }}
                 className="R-icons-top"
               >
+                
                 {length}
               </Box>
               
@@ -152,6 +126,59 @@ function Navbar() {
                 <span className="R-Text">Cart</span>
               </p>
             </Flex>
+          <Flex color={"white"} ml={"30px"} gap={2}>
+          
+            <Center gap={2}> 
+            <Person2RoundedIcon  /> 
+            <p className="R-Text" display={{base:"none", md:"block"}} style={{ cursor: "pointer" }} onClick={handleLogin}>
+              {val || "Sign in/ Sign Up"}
+            
+            </p>        
+            {val!=null?
+              <Menu>
+  <MenuButton
+    as={IconButton}
+    aria-label='Options'
+    icon={ <AiOutlineDown/> }
+    variant='outline'
+  />
+  <MenuList  p="0px" boxShadow="rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px">
+  <Box
+                      borderRadius="5px 5px 0% 0%"
+                      bgColor="#008ecc"
+                      h="60px"
+                      display={"flex"}
+                      alignItems="center"
+                      justifyContent="space-around"
+                      gap={5}
+                    >
+                      <Center gap={35}>
+                      <FaIcons.FaUserCircle  fontSize="35px" color="#fff" />
+
+                      <Text color="#fff" fontWeight="bold">
+                        {val.toUpperCase()}
+                      </Text>
+                      </Center>
+                    </Box>
+    <MenuItem icon={<AddIcon />} >
+      My Order
+    </MenuItem>
+    <MenuItem icon={<FiShoppingCart />} onClick={() => navigate("/cart")} >
+      My Cart
+    </MenuItem>
+    <MenuItem icon={<RiAdminLine />} onClick={() => navigate("/admin")} >
+      Admin Panel
+    </MenuItem>
+    <MenuItem bgColor="#d63e3e"
+                      h="30px" icon={<BiLogOut />} onClick={() => handleLogout()} >
+      LogOut
+    </MenuItem>
+  </MenuList>
+</Menu>:""}
+
+</Center>
+ 
+         
           </Flex>
         </Flex>
       </Box>
